9bf0ed9f147efb3d6db10e61ad2e53fc577466c2
David Bogas <david.bogas@braunistudio.com>
2024-09-10 19:04:10 +0200

    Added locale support in settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,13 +117,24 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
+LANGUAGES = [
+    ('es', 'Español'),
+    ('en', 'English'),
+]
+
 TIME_ZONE = 'Europe/Madrid'
 
 DATE_INPUT_FORMATS = ('%d-%m-%Y','%Y-%m-%d')
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
